ed7ad3f70cc6e257f8d1ed6aa266150c7408b640
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-19 09:21:59 +0600

    update project/resources/views/Frontend/include/Preloader.blade.php

--- project/resources/views/Frontend/include/Preloader.blade.php
+++ project/resources/views/Frontend/include/Preloader.blade.php
@@ -1,4 +1,4 @@
-@vite('resources/css/Preloader.css')
+<link rel="stylesheet" href="{{ asset('public/css/Preloader.css') }}">
 <div class='preloaderdiv' id="preloader">
   <div>
     <img src="/storage/{{$webInfo->logo}}" class="Preloaderimg" style="">
