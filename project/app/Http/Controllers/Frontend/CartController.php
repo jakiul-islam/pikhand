da4233b3adcadea98757b332673896bbff396d45
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-19 09:11:38 +0600

    update project/app/Http/Controllers/Frontend/CartController.php

--- project/app/Http/Controllers/Frontend/CartController.php
+++ project/app/Http/Controllers/Frontend/CartController.php
@@ -69,7 +69,10 @@ class CartController extends Controller
             "quantity"      => 1
           ];
           
-          
+          return response()->json([
+            'ststus' => false,
+            'message'=>'Products add to cart',
+          ],200);
           
         }
         session()->put('cart', $cart);
