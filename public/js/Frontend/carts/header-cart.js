a0378cad78e5280d26a729d13afbf548e1045717
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-20 08:51:40 +0600

    update public/js/Frontend/carts/header-cart.js

--- public/js/Frontend/carts/header-cart.js
+++ public/js/Frontend/carts/header-cart.js
@@ -404,10 +404,13 @@
   }
   // usershownotise();
   window.addCart = function(productId,cartPrice){
-  let formData = new FormData();
-    formData.append('productId',productId);
-    formData.append('cartPrice',cartPrice);
-    sendDataAjax('/cart/create',formData,'post','addCartForGuestUser','Nan','Nan','Nan','Nan');
+    let loginOrnotFor = document.getElementById("loginOrnotFor").value;
+  
+  
+    let formData = new FormData();
+      formData.append('productId',productId);
+      formData.append('cartPrice',cartPrice);
+      sendDataAjax('/cart/create',formData,'post','addCartForGuestUser','Nan','Nan','Nan','Nan');
   }
   window.addCartForGuestUser = function( response ){
     if(response.user == 'guest'){
