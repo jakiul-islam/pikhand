cace187d85de7a652d3a7e52952c957ad9b5ddb9
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-20 14:41:07 +0600

    update public/js/Frontend/carts/header-cart.js

--- public/js/Frontend/carts/header-cart.js
+++ public/js/Frontend/carts/header-cart.js
@@ -21,9 +21,9 @@
       countcarts.innerText= cart.length;
       if(cart.length === 0){
           cart.forEach(function(carts) {
-          //  let cartPrice  =`${carts.product_price }`;
-          //  let cartid     =`${carts.id}`;
-          //  let quantity   =`${carts.quantity}`;
+            let cartPrice  =`${carts.product_price }`;
+            let cartid     =`${carts.id}`;
+            let quantity   =`${carts.quantity}`;
              //কষকদকদ
 
             //cart product show
