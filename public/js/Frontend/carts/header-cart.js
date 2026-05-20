fd1bc4ac3cd052a82d586facc79c5ecdf2231627
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-20 13:36:52 +0600

    update public/js/Frontend/carts/header-cart.js

--- public/js/Frontend/carts/header-cart.js
+++ public/js/Frontend/carts/header-cart.js
@@ -419,7 +419,9 @@
          quantity: 1 
       });
       
-      localStorage.setItem('cart', JSON.stringify(cart));
+      localStorage.setItem("cart",JSON.stringify(cart)
+      
+     // localStorage.setItem('cart', JSON.stringify(cart));
      //console.log( localStorage.getItem('cart') );
       
     }else{
