d2e9a1b3769fc6ea2410c8908830ccfcaeb25ad0
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-20 09:40:37 +0600

    update public/js/Frontend/carts/header-cart.js

--- public/js/Frontend/carts/header-cart.js
+++ public/js/Frontend/carts/header-cart.js
@@ -406,6 +406,10 @@
   window.addCart = function(productId,cartPrice){
     let loginOrnotFor = document.getElementById("loginOrnotFor").value;
     if(loginOrnotFor == 'userNotLogin'){
+      
+      
+      
+      
       let exists = cart.some(item => item.id === productId);
       if (exists) {
         // quantity বাড়াও
