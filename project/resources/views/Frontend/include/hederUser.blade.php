649ee91c49ca78909c62ea3920cce24483fde957
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-23 22:50:46 +0600

    update project/resources/views/Frontend/include/hederUser.blade.php

--- project/resources/views/Frontend/include/hederUser.blade.php
+++ project/resources/views/Frontend/include/hederUser.blade.php
@@ -136,7 +136,24 @@
        <!-- href='/home/chackout' -->
      </div>
    </div>
-
-
-
  <!--  end carts offcanvas -->
+
+<!-- user login for checkuot  -->
+ <div class="modal fade" id="categoryForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Create new category</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body row">
+          </div>
+          <div class="modal-footer">
+            <button type="reset" id="insertclose" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" id="insertCatagory" class="btn btn-primary">Create</button>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
+
