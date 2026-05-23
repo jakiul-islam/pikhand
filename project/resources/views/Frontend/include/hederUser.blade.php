dcb3f5fcb18b1b19e07cb91bf64d72b20b3012f9
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-23 23:06:00 +0600

    update project/resources/views/Frontend/include/hederUser.blade.php

--- project/resources/views/Frontend/include/hederUser.blade.php
+++ project/resources/views/Frontend/include/hederUser.blade.php
@@ -142,7 +142,7 @@
  <div class="modal fade" id="categoryForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
-          <ul class="navbar-nav justify-content-start flex-grow-1 pe-3"  id="userInfo">
+          <ul class="navbar-nav justify-content-start flex-grow-1 pe-3"  id="">
           </ul>
         </div>
       </div>
