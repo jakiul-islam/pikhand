54f4d39fcf0e87f1dc38dea4cdd0e5010988316a
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-23 23:25:11 +0600

    update project/resources/views/Frontend/include/hederUser.blade.php

--- project/resources/views/Frontend/include/hederUser.blade.php
+++ project/resources/views/Frontend/include/hederUser.blade.php
@@ -142,7 +142,7 @@
  <div class="modal fade" id="categoryForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-header">
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-content">
         <div class="modal-body">
