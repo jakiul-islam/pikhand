5b750483dfe539e5c9935923bef66e4897e2af5d
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-30 23:59:33 +0600

    update project/resources/views/Admin/category-subcategory/category.blade.php

--- project/resources/views/Admin/category-subcategory/category.blade.php
+++ project/resources/views/Admin/category-subcategory/category.blade.php
@@ -6,8 +6,8 @@
     <title>jis food admin panale</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="{{ asset('public/css/Admin/common.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/css/Admin/Category.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Common.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Sategory.css') }}">
     <link rel="stylesheet" href="{{ asset('public/css/Admin/subcategory.css') }}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
