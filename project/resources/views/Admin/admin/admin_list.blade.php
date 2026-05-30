57760927ca0445713bf7135987e48629936496be
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-05-30 09:16:36 +0600

    update project/resources/views/Admin/admin/admin_list.blade.php

--- project/resources/views/Admin/admin/admin_list.blade.php
+++ project/resources/views/Admin/admin/admin_list.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>jis food admin panale</title>
-    <link rel="stylesheet" href="{{ asset('css/Admin/Common.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Admin/Admin-Details.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Common.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Admin-Details.css') }}">
     
     <!-- icone link -->    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!--bootstrap link-->  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
