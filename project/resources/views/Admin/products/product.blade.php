10f1211e40d1e615b810fb5f3be962512225d20c
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-02 01:32:15 +0600

    update project/resources/views/Admin/products/product.blade.php

--- project/resources/views/Admin/products/product.blade.php
+++ project/resources/views/Admin/products/product.blade.php
@@ -356,13 +356,13 @@
     @include("Admin.products.product_img_add")
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    @vite([
-        'resources/js/Admin/common.js',
-        'resources/js/Admin/product/product.js',
-        'resources/js/Admin/product/product-details.js',
-        'resources/js/Admin/product/product-img.js',
-        'resources/js/Admin/product/product-update.js'
-    ])
+      <script src="{{ asset('public/js/Admin/common.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/product/product.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/product/product-details.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/product/product-img.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/product/product-update.js') }} " ></script>
+
+     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
