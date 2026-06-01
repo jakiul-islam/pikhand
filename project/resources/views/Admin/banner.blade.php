d73f634cd1fbe7cb5542c43d34ee3b572a88d80e
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-02 01:15:13 +0600

    update project/resources/views/Admin/banner.blade.php

--- project/resources/views/Admin/banner.blade.php
+++ project/resources/views/Admin/banner.blade.php
@@ -99,9 +99,7 @@
         </div>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    @vite([
-        'resources/js/Admin/common.js',
-        'resources/js/Admin/banner-managements.js'
-    ])
+   <script src="{{ asset('public/js/Admin/common.js') }} " ></script>
+    <script src="{{ asset('public/js/Admin/banner-managements.js') }}"></script>
   </body>
 </html>
