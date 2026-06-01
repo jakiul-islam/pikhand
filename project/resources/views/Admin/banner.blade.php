ebe403bf73fbe3fd1a968cec5cf9d599de3ae0f2
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-02 01:16:10 +0600

    update project/resources/views/Admin/banner.blade.php

--- project/resources/views/Admin/banner.blade.php
+++ project/resources/views/Admin/banner.blade.php
@@ -28,7 +28,7 @@
   </head>
 
   <body>
-   @include("Admin.include.header")
+   @include("Admin.include.Header")
     <div class="main-contain">
       <div class="name-2">
         <h1>Insert banner</h1>
