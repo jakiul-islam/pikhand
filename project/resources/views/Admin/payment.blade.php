36b068e88df0cdbcca98b8eed3a8bc17db5e8d2c
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:43:12 +0600

    update project/resources/views/Admin/payment.blade.php

--- project/resources/views/Admin/payment.blade.php
+++ project/resources/views/Admin/payment.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>jis food admin panale</title>
-    <link rel="stylesheet" href="{{ asset('css/Admin/Common.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Admin/User.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Common.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/User.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
   </head>
   
@@ -94,7 +94,7 @@
    
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/Admin/common.js') }}"></script>
-    <script src="{{ asset('js/Admin/payment.js') }}"></script>
+    <script src="{{ asset('public/js/Admin/common.js') }}"></script>
+    <script src="{{ asset('public/js/Admin/payment.js') }}"></script>
   </body>
 </html>
