f0ec315d19996972eefc8f1bc244059a32084958
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:59:41 +0600

    update project/resources/views/Admin/voucher.blade.php

--- project/resources/views/Admin/voucher.blade.php
+++ project/resources/views/Admin/voucher.blade.php
@@ -143,5 +143,5 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="{{ asset('publc/js/Admin/common.js') }}"></script>
-        <script src="{{ asset('publc/js/Admin/Voucher.js') }}"></script>
+        <script src="{{ asset('public/js/Admin/common.js') }}"></script>
+        <script src="{{ asset('public/js/Admin/Voucher.js') }}"></script>
