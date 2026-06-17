879cad8fa102050b980a2497a29ee8372edf1315
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 12:36:09 +0600

    update project/resources/views/Admin/dashboard/dashboard.blade.php

--- project/resources/views/Admin/dashboard/dashboard.blade.php
+++ project/resources/views/Admin/dashboard/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>jis food admin panale</title>
-    @vite('resources/css/Admin/Common.css')
+        <link rel="stylesheet" href="{{ asset('public/css/Admin/Common.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
    <!-- <script type="text/javascript"
