679f85e75b53afc5b0c3cf0661720bc04e17f21c
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 14:15:49 +0600

    update project/routes/web.php

--- project/routes/web.php
+++ project/routes/web.php
@@ -105,7 +105,7 @@ Route::middleware([CheckAdmin::class])->group(function () {
   Route::get('/admin/Brand', function () {return view('Admin.brand'); });
   Route::get('/admin/Vouchers/', function () {return view('Admin.voucher'); })->name('admin.Voucher');
   Route::get('/admin/Help/', function () {return view('Admin.Help'); })->name('admin.Help');
-  Route::get('/admin/Policies/', function () {return view('Admin.Policies'); })->name('admin.Policies');
+  Route::get('/admin/Policies/', function () {return view('Admin.policies'); })->name('admin.Policies');
   Route::get('/admin/About/', function () {return view('Admin.about'); })->name('admin.About');
 
 
