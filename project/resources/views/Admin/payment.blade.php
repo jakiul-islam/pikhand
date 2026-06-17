8164ec202e4a3fb2ad9a470b248e54404a206a40
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:44:03 +0600

    update project/resources/views/Admin/payment.blade.php

--- project/resources/views/Admin/payment.blade.php
+++ project/resources/views/Admin/payment.blade.php
@@ -21,7 +21,7 @@
   </style>
   
 <body>
-  @include("Admin.Include.header")
+  @include("Admin.Include.Header")
   <div class="main-contain" id="maindiv">
     <div class="search-fillter">
       <div class="div-search">
