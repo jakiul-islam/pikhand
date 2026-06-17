4669a247871fa1cad8d44f5cacf818bc9b0774e1
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:28:56 +0600

    update project/resources/views/Admin/cart.blade.php

--- project/resources/views/Admin/cart.blade.php
+++ project/resources/views/Admin/cart.blade.php
@@ -60,8 +60,8 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
    
-          <script src="{{ asset('public/js/Admin/common.js') }} " ></script>
-          <script src="{{ asset('public/js/Admin/Cart.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/common.js') }} " ></script>
+      <script src="{{ asset('public/js/Admin/Cart.js') }} " ></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
