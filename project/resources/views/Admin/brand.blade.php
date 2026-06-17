6fcfacdc880fbbe189a1eb531973c738efb3aede
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:47:26 +0600

    update project/resources/views/Admin/brand.blade.php

--- project/resources/views/Admin/brand.blade.php
+++ project/resources/views/Admin/brand.blade.php
@@ -7,7 +7,7 @@
     <title>jis food admin panale</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    @vite('resources/css/Admin/Common.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/Admin/Common.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="{{ asset('tinymce/tinymce.min.js') }}"></script>
     <script>
