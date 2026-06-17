9a4dd4a9157d17ee02ab5c6a4492eabe4924c8c6
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-17 13:36:31 +0600

    update project/resources/views/Admin/admin/admin_list.blade.php

--- project/resources/views/Admin/admin/admin_list.blade.php
+++ project/resources/views/Admin/admin/admin_list.blade.php
@@ -102,9 +102,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <!--jQuery link-->
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script  src="{{ asset('js/Admin/common.js') }}"></script>
-        <script  src="{{ asset('js/Admin/admin/admin-list.js') }}"></script>
-        <script  src="{{ asset('js/Admin/admin/admin-page-list.js') }}"></script>
-        <script  src="{{ asset('js/Admin/admin/admin-profile-details.js') }}"></script>
+        <script  src="{{ asset('public/js/Admin/common.js') }}"></script>
+        <script  src="{{ asset('public/js/Admin/admin/admin-list.js') }}"></script>
+        <script  src="{{ asset('public/js/Admin/admin/admin-page-list.js') }}"></script>
+        <script  src="{{ asset('public/js/Admin/admin/admin-profile-details.js') }}"></script>
   </body>
 </html>
