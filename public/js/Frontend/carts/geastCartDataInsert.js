9e3900cc0715b5b6d45155ba81490190f4ba4b9c
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-06-21 10:36:10 +0600

    update public/js/Frontend/carts/geastCartDataInsert.js

--- public/js/Frontend/carts/geastCartDataInsert.js
+++ public/js/Frontend/carts/geastCartDataInsert.js
@@ -1,6 +1,18 @@
-window.FetchCarts = function(){
+  window.FetchCarts = function(){
   
-  
-  
-  
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      countcarts.innerText= cart.length;
+      if(cart.length > 0){
+          cart.forEach(function(carts) {
+            
+            let cartPrice  =`${carts.price }`;
+            let cartid     =`${carts.id}`;
+            let quantity   =`${carts.quantity}`;
+             //কষকদকদ
+
+            //cart product show
+            let formData = new FormData();
+            formData.append('productId',carts.id);
+          })
+      }
 }
