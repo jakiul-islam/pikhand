b81f05c6357cb65369139f3394fc73e9c6c46855
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 00:26:38 +0600

    update project/resources/views/Admin/banner.blade.php

--- project/resources/views/Admin/banner.blade.php
+++ project/resources/views/Admin/banner.blade.php
@@ -109,7 +109,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <h1 class='text-center'>${banners.name}</h1>
+            <h1 class='text-center' id='bannerName'></h1>
             <p>id:${banners.id}</p>
             <p>${banners.slug}</p>
             <p>${banners.description}</p>
