27b96107ad76de05bd8576c701251836cad7389d
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 00:41:55 +0600

    update public/js/Admin/banner-managements.js

--- public/js/Admin/banner-managements.js
+++ public/js/Admin/banner-managements.js
@@ -52,7 +52,7 @@
                 <td>${banners.slug}</td>
                 <td>${banners.description}</td>
                 <td><img src="/storage/${banners.image}" width="100" alt="${banners.name}"></td>
-                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#viewModel">View</button></td>
+                <td><button type="button" onclick="viewDateSet('${banners.id}','${banners.name}','${banners.slug}','${banners.description}','${banners.image}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#viewModel">View</button></td>
                 <td><button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#EditBannerModel">edite</button></td>
                 <td><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteBannerModel">delete</button></td>
               </tr>
