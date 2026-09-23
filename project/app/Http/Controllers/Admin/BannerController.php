41bb7c22a65b4b4edd4d6bcd3afcee7aba490caf
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-23 23:06:23 +0600

    update project/app/Http/Controllers/Admin/BannerController.php

--- project/app/Http/Controllers/Admin/BannerController.php
+++ project/app/Http/Controllers/Admin/BannerController.php
@@ -54,31 +54,6 @@ class BannerController extends Controller
     // Width সর্বোচ্চ 1200px
     $image->scaleDown(width: 1200);
 
-    /* // নতুন filename
-    $filename = uniqid() . '.webp';
-
-    $path = storage_path('app/public/banner/' . $filename);
-
-    // directory না থাকলে তৈরি
-    if (!file_exists(dirname($path))) {
-        mkdir(dirname($path), 0755, true);
-    }
-
-    // WebP হিসেবে save
-    $image->toWebp(80)->save($path);
-
-
-
-
-
-
-
-        
-      //  $image = Image::read($file);
-
-        // সর্বোচ্চ width 1200px
-        $file->scaleDown(width: 1200);
-        */
         // WebP filename
         $filename = time() . '_' . uniqid() . '.webp';
         
