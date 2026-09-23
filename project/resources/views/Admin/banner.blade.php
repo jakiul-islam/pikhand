f89b14d7a7a38f011250b67ceea7884bb0129cb8
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 00:26:13 +0600

    update project/resources/views/Admin/banner.blade.php

--- project/resources/views/Admin/banner.blade.php
+++ project/resources/views/Admin/banner.blade.php
@@ -105,7 +105,7 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="${vewlId}Label">banner motels</h1>
+            <h1 class="modal-title fs-5" id="viewModelLabel">banner motels</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
