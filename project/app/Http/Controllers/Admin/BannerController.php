e9406cb9a301741b14870c35dd8e32c568ff97bc
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-23 21:36:28 +0600

    update project/app/Http/Controllers/Admin/BannerController.php

--- project/app/Http/Controllers/Admin/BannerController.php
+++ project/app/Http/Controllers/Admin/BannerController.php
@@ -155,8 +155,9 @@ class BannerController extends Controller
     
           $brand_delete = banner::where('id', $request->deleteId)->first();
           if ($brand_delete) {
-            $imagePath = storage_path('app/public/' . $brand_delete->logo);
-           // $imagePathpub = public_path('public/' . $brand_delete->logo);
+            //$imagePath = storage_path('app/public/' . $brand_delete->logo);
+           $imagePath = $brand_delete->logo;
+            // $imagePathpub = public_path('public/' . $brand_delete->logo);
             // ইমেজ ফাইল এক্সিস্ট করে কি না চেক করে ডিলিট করুন
            // if (File::exists($imagePath)) {
             if ($imagePath && Storage::disk('public')->exists($imagePath)) {
