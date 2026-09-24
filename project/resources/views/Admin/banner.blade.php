4ad4e8e8e3b96f773e9a5b7b7d8bb1cbfe9377a3
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 17:48:52 +0600

    update project/resources/views/Admin/banner.blade.php

--- project/resources/views/Admin/banner.blade.php
+++ project/resources/views/Admin/banner.blade.php
@@ -148,7 +148,7 @@
               <span class="input-group-text"
               id="addon-wrapping">banner description</span>
             <div class="input-group flex-nowrap">
-              <textarea id="bannersBescription" class='editedescription'>banners.description</textarea>
+              <textarea id="EditBannersDescription" class='editedescription'>banners.description</textarea>
             </div>
           </div>
           <div class="modal-footer">
