ee4f5a27ba92f08e1d3ed2cb9b883e4d565660d4
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 18:45:25 +0600

    update public/js/Admin/banner-managements.js

--- public/js/Admin/banner-managements.js
+++ public/js/Admin/banner-managements.js
@@ -135,7 +135,9 @@
 
       $(document).ready(function(){
         $(document).on("click", "#bannerDeletebutton", function(){
-          
+
+
+          let formData = new FormData();
           formData.append('deleteId', bannerDescription);
 
            sendDataAjax('/admin/deleteservices',formData,'post','fetchBanner','Nan','editebuttonbanner','Save change','EditBannerModel' );
