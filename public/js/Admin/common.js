ac365a09ac3cbbe0b390e167928358393d7ae2e0
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-25 01:23:33 +0600

    update public/js/Admin/common.js

--- public/js/Admin/common.js
+++ public/js/Admin/common.js
@@ -217,7 +217,7 @@
 
 
 //fronten img compress
-function compressWithCanvas(file) {
+window.compressWithCanvas = function(file) {
     return new Promise((resolve) => {
         const img = new Image();
         img.src = URL.createObjectURL(file);
