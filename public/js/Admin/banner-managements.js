cabac46336001640555f6f761a64ed4952a88bb7
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-24 18:14:06 +0600

    update public/js/Admin/banner-managements.js

--- public/js/Admin/banner-managements.js
+++ public/js/Admin/banner-managements.js
@@ -87,7 +87,7 @@
            bannerName.value = name;
            bannerId.value = id;
            bannerSlug.value = slug;
-          tinymce.get('EditBannersDescription').setContent(description);
+           tinymce.get('EditBannersDescription').setContent(description);
            previewImage1.src = "/storage/" +image;
           
         }
