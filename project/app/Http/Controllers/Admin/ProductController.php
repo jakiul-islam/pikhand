7a4d3f37cc51698883dd45eecfc55cc65a553c59
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-27 00:02:38 +0600

    update project/app/Http/Controllers/Admin/ProductController.php

--- project/app/Http/Controllers/Admin/ProductController.php
+++ project/app/Http/Controllers/Admin/ProductController.php
@@ -68,7 +68,7 @@ class ProductController extends Controller
           80
         );
 
-      $productChack = product::where('id',$request->id)->where('id',$request->id)->first();
+      $productChack = product::where('id',$request->name)->where('id',$request->keyword)->first();
       
       $productId = product::create([
         'name'            =>$request->name,
