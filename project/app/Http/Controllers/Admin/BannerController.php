a3aca725131c89fc4641ea04f1d1c7e229271482
Jakiul Islam <mdjakiulis4444@gmail.com>
2026-09-27 00:09:37 +0600

    update project/app/Http/Controllers/Admin/BannerController.php

--- project/app/Http/Controllers/Admin/BannerController.php
+++ project/app/Http/Controllers/Admin/BannerController.php
@@ -22,7 +22,7 @@ class BannerController extends Controller
           [
             'bannerName'         => 'required|string',
             'bannerSlog'         => 'required|string',
-            'imageInput'         => 'required|image|mimes:jpeg,png,jpg,gif,jpej,webp|max:10250',
+            'imageInput'         => 'required|image|mimes:jpeg,png,jpg,gif,jpej,webp,mp3|max:10250',
             'bannerDescription'  =>'required|string',
           ]
       );
